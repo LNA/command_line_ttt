80f77c72dc6835f8270b7c93e8b942cea0c67ab1
LNA <LAllen@8thLight.com>
2014-05-08 11:56:10 -0500

    Puts welcome message back into method that gets options

--- lib/setup.rb
+++ lib/setup.rb
@@ -6,10 +6,15 @@ class Setup
   end
 
   def get_game_options
+    welcome_message
     player_one      = player_type("one")
     player_two      = player_type("two")
     player_one_mark = gets_mark_for("one")
     player_two_mark = gets_mark_for("two")
+  end
+
+  def welcome_message
+    @ui.welcome_user
   end
 
   def player_type(number)
